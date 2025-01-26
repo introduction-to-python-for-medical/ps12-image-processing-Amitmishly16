87667c1cdfdd147f633879a0c9cb373d5053c0fa
Amitmishly16 <amitmishly@gmail.com>
2025-01-26 15:05:38 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -2,16 +2,9 @@ from image_utils import load_image, edge_detection
 from PIL import Image
 from skimage.filters import median
 from skimage.morphology import ball
-import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from scipy.signal import convolve2d
-
-def edge_detection(image_array):
-    mean_looki = image_array.mean(axis=2)
-    filtered_looki_Y = convolve2d(mean_looki, np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
-    filtered_looki_X = convolve2d(mean_looki, np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]))
-    edgeMAG = np.sqrt(filtered_looki_X**2 + filtered_looki_Y**2)
-    return edgeMAG
+import numpy as np
 
 def load_image(file_path):
     image = Image.open(file_path)
@@ -21,3 +14,12 @@ def load_image(file_path):
 file_path = '/content/looki.jpeg'
 image_array = load_image(file_path)
 print(image_array.shape)
+
+def edge_detection(image_array):
+    mean_image = np.mean(image_array, axis=2)
+    kernelY = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    kernelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    edgeY = convolve2d(mean_image, kernelY, mode='same', boundary='symm')
+    edgeX = convolve2d(mean_image, kernelX, mode='same', boundary='symm')
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG
