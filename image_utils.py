cbd161ca2ced5e255d76db8da5832597f0ce7ff8
Amitmishly16 <amitmishly@gmail.com>
2025-01-26 13:53:17 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,23 @@
+from image_utils import load_image, edge_detection
 from PIL import Image
+from skimage.filters import median
+from skimage.morphology import ball
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
 
-def load_image(path):
-    pass # Replace the `pass` with your code
+def edge_detection(image_array):
+    mean_looki = image_array.mean(axis=2)
+    filtered_looki_Y = convolve2d(mean_looki, np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
+    filtered_looki_X = convolve2d(mean_looki, np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]))
+    edgeMAG = np.sqrt(filtered_looki_X**2 + filtered_looki_Y**2)
+    return edgeMAG
 
-def edge_detection(image):
-    pass # Replace the `pass` with your code
+def load_image(file_path):
+    image = Image.open(file_path)
+    image_array = np.array(image)
+    return image_array
+
+file_path = '/content/looki.jpeg'
+image_array = load_image(file_path)
+print(image_array.shape)
