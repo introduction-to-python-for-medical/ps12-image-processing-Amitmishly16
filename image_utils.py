bb1d169ed1760c210d0a11453fb1748528ce8225
Amitmishly16 <amitmishly@gmail.com>
2025-01-27 12:51:34 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,24 +1,27 @@
+from image_utils import load_image, edge_detection
+from PIL import Image
+from skimage.filters import median
+from skimage.morphology import ball
 import numpy as np
 from scipy.signal import convolve2d
-from PIL import Image
+import matplotlib.pyplot as plt
 
-def load_image(file_path):
-    image = Image.open(file_path) 
-    image_array = np.array(image)  # Convert the image into a NumPy array
-    
+def load_image(path):
+    image = Image.open(path)
+    image_array = np.array(image)
     return image_array
 
 def edge_detection(image_array):
-    mean_image = np.mean(image_array, axis=2)  # Convert to grayscale
+    lena_mean = np.mean(image_array, axis=2)
     
-    kernelY = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  # Vertical edges
-    kernelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  # Horizontal edges
+    kernelY = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  
+    kernelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  
     
     # Apply the filters to the grayscale image
-    edgeY = convolve2d(mean_image, kernelY)  # Vertical edges
-    edgeX = convolve2d(mean_image, kernelX)  # Horizontal edges
+    edgeY = convolve2d(lena_mean, kernelY)  # Vertical edges
+    edgeX = convolve2d(lena_mean, kernelX)  # Horizontal edges
     
     # Calculate edgeMAG
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
     
-    return edgeMAG
+    return edgeMAG 
